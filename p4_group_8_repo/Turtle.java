536b91562215fa206d2d30394953eed7d31f40fc
hfydi1 <hfydi1@nottingham.edu.my>
2020-12-15 17:34:17 +0800

    Encapsulation with getter and setter to Turtle.java

--- p4_group_8_repo/Turtle.java
+++ p4_group_8_repo/Turtle.java
@@ -3,11 +3,10 @@ package p4_group_8_repo;
 import javafx.scene.image.Image;
 
 public class Turtle extends Actor{
-	Image turtle1;
-	Image turtle2;
-	Image turtle3;
+	private Image turtle1;
+	private Image turtle2;
+	private Image turtle3;
 	private int speed;
-	int i = 1;
 	boolean bool = true;
 	@Override
 	public void act(long now) {
@@ -31,6 +30,31 @@ public class Turtle extends Actor{
 		if (getX() < -75 && speed<0)
 			setX(600);
 	}
+	
+	public Image getTurtle1() {
+		return turtle1;
+	}
+	
+	public void setTurtle1(Image turtle1) {
+		this.turtle1 = turtle1;
+	}
+	
+	public Image getTurtle2() {
+		return turtle2;
+	}
+	
+	public void setTurtle2(Image turtle2) {
+		this.turtle2 = turtle2;
+	}
+	
+	public Image getTurtle3() {
+		return turtle3;
+	}
+	
+	public void setTurtle3(Image turtle3) {
+		this.turtle3 = turtle3;
+	}
+	
 	public Turtle(int xpos, int ypos, int s, int w, int h) {
 		turtle1 = new Image("file:src/assets/img/TurtleAnimation1.png", w, h, true, true);
 		turtle2 = new Image("file:src/assets/img/TurtleAnimation2.png", w, h, true, true);
