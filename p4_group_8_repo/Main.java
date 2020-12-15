2cfbf571e8b65a869dadb43de73cd735dcdbb986
hfydi1 <hfydi1@nottingham.edu.my>
2020-12-15 17:31:30 +0800

    Adjusted log speed and obstacle speed.

--- p4_group_8_repo/Main.java
+++ p4_group_8_repo/Main.java
@@ -31,20 +31,20 @@ public class Main extends Application {
 		//Obstacle obstacle1 = new Obstacle("file:src/assets/img/truck2Right.png", 100, 100,2 );
 		//Obstacle obstacle2 = new Obstacle("file:src/assets/img/truck1Right.png",0,  150, 1);
 
-		BackgroundImage froggerback = new BackgroundImage("file:src/assets/img/bg.png");
+		BackgroundImage froggerback = new BackgroundImage("file:src/assets/img/bgtest.png");
 	    
 		background.add(froggerback);
 		
-		background.add(new Log("file:src/assets/img/log3.png", 150, 0, 166, 0.75));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 220, 166, 0.75));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 440, 166, 0.75));
+		background.add(new Log("file:src/assets/img/log3.png", 150, 0, 166, 0.2));
+		background.add(new Log("file:src/assets/img/log3.png", 150, 220, 166, 0.2));
+		background.add(new Log("file:src/assets/img/log3.png", 150, 440, 166, 0.2));
 		//background.add(new Log("file:src/assets/img/log3.png", 150, 0, 166, 0.75));
-		background.add(new Log("file:src/assets/img/logs.png", 300, 0, 276, -2));
-		background.add(new Log("file:src/assets/img/logs.png", 300, 400, 276, -2));
+		background.add(new Log("file:src/assets/img/logs.png", 300, 0, 276, -1));
+		background.add(new Log("file:src/assets/img/logs.png", 300, 400, 276, -1));
 		//background.add(new Log("file:src/assets/img/logs.png", 300, 800, 276, -2));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 50, 329, 0.75));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 270, 329, 0.75));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 490, 329, 0.75));
+		background.add(new Log("file:src/assets/img/log3.png", 150, 50, 329, 0.1));
+		background.add(new Log("file:src/assets/img/log3.png", 150, 270, 329, 0.1));
+		background.add(new Log("file:src/assets/img/log3.png", 150, 490, 329, 0.1));
 		//background.add(new Log("file:src/assets/img/log3.png", 150, 570, 329, 0.75));
 		
 		background.add(new Turtle(500, 376, -1, 130, 130));
@@ -79,17 +79,17 @@ public class Main extends Application {
 		background.add(new End(141 + 141-13+141-13+141-13+3,96));
 		animal = new Animal("file:src/assets/img/froggerUp.png");
 		background.add(animal);
-		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 0, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 300, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 600, 649, 1, 120, 120));
+		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 0, 649, 0.5, 120, 120));
+		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 300, 649, 0.5, 120, 120));
+		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 600, 649, 0.5, 120, 120));
 		//background.add(new Obstacle("file:src/assets/img/truck1"+"Right.png", 720, 649, 1, 120, 120));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 100, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 250, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 400, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 550, 597, -1, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/truck2Right.png", 0, 540, 1, 200, 200));
-		background.add(new Obstacle("file:src/assets/img/truck2Right.png", 500, 540, 1, 200, 200));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 500, 490, -5, 50, 50));
+		background.add(new Obstacle("file:src/assets/img/car1Left.png", 100, 597, -0.5, 50, 50));
+		background.add(new Obstacle("file:src/assets/img/car1Left.png", 250, 597, -0.5, 50, 50));
+		background.add(new Obstacle("file:src/assets/img/car1Left.png", 400, 597, -0.5, 50, 50));
+		background.add(new Obstacle("file:src/assets/img/car1Left.png", 550, 597, -0.5, 50, 50));
+		background.add(new Obstacle("file:src/assets/img/truck2Right.png", 0, 540, 0.5, 200, 200));
+		background.add(new Obstacle("file:src/assets/img/truck2Right.png", 500, 540, 0.5, 200, 200));
+		background.add(new Obstacle("file:src/assets/img/car1Left.png", 500, 490, -1, 50, 50));
 		background.add(new Digit(0, 30, 360, 25));
 		//background.add(obstacle);
 		//background.add(obstacle1);
