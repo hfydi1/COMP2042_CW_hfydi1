96fe582fefcf7947e5f04b0bae9ca67f79f61edd
hfydi1 <hfydi1@nottingham.edu.my>
2020-12-18 15:37:19 +0800

    Applied MVC pattern to the main game java file. All game related java files added into separate "frogger.game" package.

--- p4_group_8_repo/Main.java
+++ p4_group_8_repo/Main.java
@@ -3,6 +3,9 @@ package p4_group_8_repo;
 import java.io.File;
 import java.util.List;
 
+import frogger.game.GameController;
+import frogger.game.GameModel;
+import frogger.game.GameView;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -21,8 +24,8 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 	
-	Stage window;
-	Scene Menu, Game, Difficulty;
+	Stage mainWindow;
+	Scene Menu, Difficulty;
 	
 	AnimationTimer timer;
 	MyStage background;
@@ -35,18 +38,27 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		window = primaryStage;
+		mainWindow = primaryStage;
 		
 		//Buttons
 		//Play Game Button 
 		Label label1 = new Label("Welcome to Frogger Game!");
 		Button button1 = new Button("Go to game");
-		button1.setOnAction(e -> window.setScene(Game));
+		button1.setOnAction(e -> {
+			GameModel gamemodel = new GameModel(mainWindow);
+			GameView gameview = new GameView();
+			GameController gamecontroller = new GameController(gameview, gamemodel);
+			try {
+				gamecontroller.StartGame();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
 		
 		//Change Difficulty Button
 		Label label2 = new Label("Change game difficulty:");
 		Button button2 = new Button("Change");
-		button2.setOnAction(e -> window.setScene(Difficulty));	
+		button2.setOnAction(e -> mainWindow.setScene(Difficulty));	
 		
 		//Change Difficulty Button (Easy)
 		Label label3 = new Label("EASY");
@@ -63,6 +75,11 @@ public class Main extends Application {
 		Button button5 = new Button("Change");
 		button5.setOnAction(e -> AlertBox.display("Difficulty Changed!", "Game difficulty set to Hard."));	
 		
+		//Change Difficulty Button (Hard)
+		Label label6 = new Label("PLAY GAME");
+		Button button6 = new Button("PLAY");
+		button6.setOnAction(e -> mainWindow.setScene(Difficulty));	
+		
 		//Scenes
 		//Main Menu layout
 		VBox mainMenu = new VBox(50);
@@ -71,102 +88,26 @@ public class Main extends Application {
 		
 		//Change difficulty layout
 		VBox changeDifficulty = new VBox(50);
-		changeDifficulty.getChildren().addAll(label3, button3, label4, button4, label5, button5);
+		changeDifficulty.getChildren().addAll(label3, button3, label4, button4, label5, button5, label6, button6);
 		Difficulty = new Scene(changeDifficulty, 600, 800);
 		
 		//Game layout
-		background = new MyStage();
-	    Game  = new Scene(background,600,800);
+	    //Game  = new Scene(background,600,800);
 	    
-	    window.setScene(Menu);
-	    window.setTitle("Frogger Game");
-		window.show();
-		window.setResizable(false); //prevent user from resizing the game window that will introduce scaling errors
+	    mainWindow.setScene(Menu);
+	    mainWindow.setTitle("Frogger Game");
+		mainWindow.show();
+		mainWindow.setResizable(false); //prevent user from resizing the game window that will introduce scaling errors
 		
-		BackgroundImage froggerback = new BackgroundImage("file:src/assets/img/bgtest.png");
-	    
-		background.add(froggerback);
-		
-		background.add(new Log("file:src/assets/img/log3.png", 150, 0, 166, 0.2));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 220, 166, 0.2));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 440, 166, 0.2));
-		background.add(new Log("file:src/assets/img/logs.png", 300, 0, 276, -1));
-		background.add(new Log("file:src/assets/img/logs.png", 300, 400, 276, -1));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 50, 329, 0.2));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 270, 329, 0.2));
-		background.add(new Log("file:src/assets/img/log3.png", 150, 490, 329, 0.2));
-		
-		background.add(new Turtle(500, 376, -1, 130, 130));
-		background.add(new Turtle(300, 376, -1, 130, 130));
-		background.add(new WetTurtle(700, 376, -1, 130, 130));
-		background.add(new WetTurtle(600, 217, -1, 130, 130));
-		background.add(new WetTurtle(400, 217, -1, 130, 130));
-		background.add(new WetTurtle(200, 217, -1, 130, 130));
+		//Difficulty speed modifier
+		Difficulty game = new Difficulty();
+		double logSpeed1 = game.getLogSpeed1();
+		double logSpeed2 = game.getLogSpeed2();
+		int turtleSpeed = game.getTurtleSpeed();
+		double obstacleSpeed = game.getObstacleSpeed();
+	
 
-		background.add(new End(13, 96)); // End represent the boxes at the end of the map
-        background.add(new End(141, 96));
-        background.add(new End(269, 96));
-        background.add(new End(398, 96));
-        background.add(new End(528, 96));
 		
-		animal = new Animal("file:src/assets/img/froggerUp.png");
-		
-		background.add(animal);
-		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 0, 649, 0.5, 120, 120));
-		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 300, 649, 0.5, 120, 120));
-		background.add(new Obstacle("file:src/assets/img/truck1Right.png", 600, 649, 0.5, 120, 120));
-		
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 100, 597, -0.5, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 250, 597, -0.5, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 400, 597, -0.5, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 550, 597, -0.5, 50, 50));
-		background.add(new Obstacle("file:src/assets/img/truck2Right.png", 0, 540, 0.5, 200, 200));
-		background.add(new Obstacle("file:src/assets/img/truck2Right.png", 500, 540, 0.5, 200, 200));
-		background.add(new Obstacle("file:src/assets/img/car1Left.png", 500, 490, -1, 50, 50));
-		background.add(new Digit(0, 30, 540, 25));
-		background.start();
-		
-		start();  
 	}
-	public void createTimer() {
-        timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-            	if (animal.changeScore()) {
-            		setNumber(animal.getPoints());
-            	}
-            	if (animal.getStop()) {
-            		System.out.print("STOPP:");
-            		background.stopMusic();
-            		stop();
-            		background.stop();
-            		Alert alert = new Alert(AlertType.INFORMATION);
-            		alert.setTitle("You Have Won The Game!");
-            		alert.setHeaderText("Your High Score: "+animal.getPoints()+"!");
-            		alert.setContentText("Highest Possible Score: 800");
-            		alert.show();
-            	}
-            }
-        };
-    }
-	public void start() {
-		background.playMusic();
-    	createTimer();
-        timer.start();
-    }
 
-    public void stop() {
-        timer.stop();
-    }
-    
-    public void setNumber(int n) {
-    	int shift = 0;
-    	while (n > 0) {
-    		  int d = n / 10;
-    		  int k = n - d * 10;
-    		  n = d;
-    		  background.add(new Digit(k, 30, 540 - shift, 25));
-    		  shift+=30;
-    		}
-    }
 }
